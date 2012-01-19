5532a2764b6ac0ec840cb9e88d6f1b6e22fb7ad0
Doug Harris <dharris@gmail.com>
2012-01-19 11:55:33 -0500

    Interim commit with the (commented) code to determine desired width

--- text-columns.js
+++ text-columns.js
@@ -1,25 +1,50 @@
 split_text = function() {
-  var t = window.getSelection();
-  var r = t.getRangeAt();
-  var node = r.commonAncestorContainer;
-  while (! node.style && node.parentElement) {
-	node = node.parentElement;
-  }
-  if (node) {
-	node.style.setProperty("-webkit-column-width", "25em");
-	node.style.setProperty("-webkit-column-gap", "1.5em");
+	var nodewidth;
+	var fontsize, size_text;
+	var required_width, avail_width;
+	var colwidth, colcount;
+	var trycount = 0;
 
-  }
+	var r = window.getSelection().getRangeAt();
+	var node = r.commonAncestorContainer;
+
+	while (! node.style && node.parentElement) {
+		node = node.parentElement;
+	}
+	if (node) {
+		node.style.setProperty("-webkit-column-count", "2");
+		/***
+		do {
+			trycount++;
+			nodewidth = node.offsetWidth;
+			size_text = document.defaultView.getComputedStyle(node).getPropertyCSSValue('font-size').cssText;
+			colcount = document.defaultView.getComputedStyle(node).getPropertyCSSValue('-webkit-column-count').cssText;
+			fontsize = size_text.replace(/\D+/,'');
+			
+			required_width=25*fontsize;
+			avail_width=(nodewidth-(3*fontsize))/2;
+			if (required_width < avail_width) {
+				colwidth="25em";
+			} else {
+				colwidth=avail_width+"px";
+			}
+			node.style.setProperty("-webkit-column-gap", "1.5em");
+			node.style.setProperty("-webkit-column-width", colwidth);
+			// setting column width sometimes changes nodewidth
+		} while (trycount < 5 && node.offsetWidth != nodewidth);
+		***/
+
+	}
 }
 
 
 chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-	if (request.action == "split") {
-	  split_text();
-	}
-    sendResponse({}); 
-}); 
+	function(request, sender, sendResponse) {
+		if (request.action == "split") {
+			split_text();
+		}
+		sendResponse({}); 
+	}); 
 
 
 

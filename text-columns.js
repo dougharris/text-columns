0fb85f697d3eb1ea5cb862acc384d73dcb3fe601
Doug Harris <dharris@gmail.com>
2012-01-19 11:57:27 -0500

    Reduced to simply use -webkit-column-count. This seems to work more reliably than it did when I first created this extension.

--- text-columns.js
+++ text-columns.js
@@ -1,10 +1,4 @@
 split_text = function() {
-	var nodewidth;
-	var fontsize, size_text;
-	var required_width, avail_width;
-	var colwidth, colcount;
-	var trycount = 0;
-
 	var r = window.getSelection().getRangeAt();
 	var node = r.commonAncestorContainer;
 
@@ -13,27 +7,6 @@ split_text = function() {
 	}
 	if (node) {
 		node.style.setProperty("-webkit-column-count", "2");
-		/***
-		do {
-			trycount++;
-			nodewidth = node.offsetWidth;
-			size_text = document.defaultView.getComputedStyle(node).getPropertyCSSValue('font-size').cssText;
-			colcount = document.defaultView.getComputedStyle(node).getPropertyCSSValue('-webkit-column-count').cssText;
-			fontsize = size_text.replace(/\D+/,'');
-			
-			required_width=25*fontsize;
-			avail_width=(nodewidth-(3*fontsize))/2;
-			if (required_width < avail_width) {
-				colwidth="25em";
-			} else {
-				colwidth=avail_width+"px";
-			}
-			node.style.setProperty("-webkit-column-gap", "1.5em");
-			node.style.setProperty("-webkit-column-width", colwidth);
-			// setting column width sometimes changes nodewidth
-		} while (trycount < 5 && node.offsetWidth != nodewidth);
-		***/
-
 	}
 }
 

31f04c0c90c0c2a74ab376b792c7e6b57e1e3d04
Doug Harris <doug.harris@slate.com>
2015-05-26 16:17:34 -0400

    - Chrome's selection doesn't always have a range... account for that. - Bump version number

--- text-columns.js
+++ text-columns.js
@@ -1,22 +1,25 @@
 
 var split_text = function() {
-	var r = window.getSelection().getRangeAt(0);
-	var node = r.commonAncestorContainer;
+  var selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    var r = selection.getRangeAt(0);
+    var node = r.commonAncestorContainer;
 
-	while (! node.style && node.parentElement) {
-		node = node.parentElement;
-	}
-	if (node) {
-		node.style.setProperty("-webkit-column-count", "2");
-	}
-	window.getSelection().collapseToStart();
+    while (! node.style && node.parentElement) {
+	  node = node.parentElement;
+    }
+    if (node) {
+	  node.style.setProperty("-webkit-column-count", "2");
+    }
+    window.getSelection().collapseToStart();
+  }
 }
 
 
 chrome.extension.onRequest.addListener(
-	function(request, sender, sendResponse) {
-		if (request.action == "split") {
-			split_text();
-		}
-		sendResponse({}); 
-	}); 
+  function(request, sender, sendResponse) {
+	if (request.action == "split") {
+	  split_text();
+	}
+	sendResponse({}); 
+  }); 
